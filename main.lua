04c31b1221582c6ec763ff422ba726161f01e163
ngthzuan <157598689+ngthzuan@users.noreply.github.com>
2024-01-24 18:42:20 +0700

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,7 @@
+wait(1)
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+    local create = local create = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+-- Tab in this --
+local Window = OrionLib:MakeWindow({Name = "ThZuan HUB (BETA)", IntroText = "WANG LIBARY", HidePremium = false, SaveConfig = true, ConfigFolder = "ngthzuanscript"})
+
