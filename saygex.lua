c84c09f3bb3d73c54912f117c69db371f4a0d5b5
ngthzuan <157598689+ngthzuan@users.noreply.github.com>
2024-12-17 13:00:25 +0700

    Create saygex.lua

---
+++ saygex.lua
@@ -0,0 +1,4 @@
+getgenv().Key = "MARU-M04UR-GT35A-HP6O-0T9X7-MO50"
+getgenv().id = "913295359550312498"
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
